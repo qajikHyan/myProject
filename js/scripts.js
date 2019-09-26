0f97d2181e720d27b5ae5981d7a3120c0b074b53
q-hovhannisyan <qajik.hovhannisyan100@gmail.com>
2019-09-26 14:43:46 +0400

    add 2 section

--- js/scripts.js
+++ js/scripts.js
@@ -19,8 +19,11 @@ $(document).ready(function() {
 		$('.menuWrapper').removeClass('open-menuWrapper')
 	})
 
-
+	$(".menu").on("swipe",function() {
+		console.log('kkkkkk')
+	 });
 	const $menu = $('.menuWrapper');
+
 
 	$(document).mouseup(e => {
 		if (!$menu.is(e.target)
@@ -31,5 +34,49 @@ $(document).ready(function() {
 
 		}
 	});
+
+	$('.btn1').click(function(e){
+		e.preventDefault();
+		$('.modal_singl-up').addClass('open_modal_singl-up');
+	})
+	$('.sign_close').click(function(e){
+		$('.modal_singl-up').removeClass('open_modal_singl-up');
+	})
+
+	function validateEmail(email) {
+		var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return re.test(email);
+	}
+	  
+	function validate() {
+		var email = $("#mail").val();
+	  
+		if (!validateEmail(email)) {
+			$("#mail").css('borderColor','red');
+		} else {
+			$("#mail").css('borderColor','#47c690');
+		}
+		return false;
+	}
+	  
+	$("#mail").on("input", validate);
+	$(".sub").on("click", validate);
+
+	$(".sub").click(function(){
+		if($('#name').val() == "" && $('#phone').val() == ""){
+			$('#name').css('borderColor','red');
+			$('#phone').parent().css('borderColor','red');
+		}else{
+			$("#name").css('borderColor','#47c690');
+			$('#phone').parent().css('borderColor','#47c690');
+		}
+	})
+	$('#name').on("input", function(){
+		if($('#name').val() == ""){
+			$('#name').css('borderColor','red');
+		}else{
+			$("#name").css('borderColor','#47c690');
+		}
+	})
 
 });
